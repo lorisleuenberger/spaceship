f7dfcc0787ae2fa3d66212bd19d4904ad30bf902
Loris Leuenberger <loris.n.leuenberger@gmail.com>
2024-10-26 19:33:09 +0200

    basic movement (without rendering)

--- game.js
+++ game.js
@@ -3,15 +3,32 @@ let game = {
     intervalId: null,
     settings: {
         keybindings: {
-            left: ['KeyA','ArrowLeft'],
-            right: ['KeyD','ArrowRight'],
+            start: ['Enter'],
+            pause: ['Escape'],
+            left: ['a'],
+            right: ['d'],
         },
     },
     startbtn: document.getElementById("start"),
     canvas: document.getElementById("canvas"),
+    player: {
+        position: 280,
+        moving: {
+            left: false,
+            right: false,
+        },
+    },
 }
-const player = {
-    position: 280,
+
+function run() {
+    if(game.player.moving.left && !game.player.moving.right) {
+        console.log("moving left")
+    } else if(game.player.moving.right && !game.player.moving.left) {
+        console.log("moving right")
+    } else {
+        console.log("standing still")
+    }
+    draw();
 }
 
 function draw() {
@@ -22,7 +39,6 @@ function draw() {
     const ctx = game.canvas.getContext("2d")
     ctx.fillStyle = "rgb(200 0 0)"
     ctx.fillRect(280,740,40,40)
-    console.log("draw")
 }
 
 function start() {
@@ -32,11 +48,12 @@ function start() {
     }
     game.startbtn.style.display = 'none'
     game.canvas.style.display = 'block'
-    game.intervalId = setInterval( draw , 20 )
+    game.intervalId = setInterval( run , 20 )
     game.running = true
 }
 
-function stop() {
+
+function pause() {
     if(!game.running) {
         console.log("game not running")
         return
@@ -49,9 +66,30 @@ function stop() {
 
 function setup() {
     game.startbtn.addEventListener('click', () => start())
-    window.addEventListener("keypress", (event) => {
-        if (event.key === "Enter") {
+    window.addEventListener('keyup', (event) => {
+        if (game.settings.keybindings.start.includes(event.key)) {
             start()
+        }
+        if (game.settings.keybindings.pause.includes(event.key)) {
+            pause()
+        }
+        if (game.running) {
+            if (game.settings.keybindings.left.includes(event.key)) {
+                game.player.moving.left = false
+            }
+            if (game.settings.keybindings.right.includes(event.key)) {
+                game.player.moving.right = false
+            }
+        }
+    })
+    window.addEventListener('keydown', (event) => {
+        if (game.running) {
+            if (game.settings.keybindings.left.includes(event.key)) {
+                game.player.moving.left = true
+            }
+            if (game.settings.keybindings.right.includes(event.key)) {
+                game.player.moving.right = true
+            }
         }
     })
 }
